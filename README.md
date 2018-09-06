cea1b049d512648c4ccf378e2426eda9e23c9d07
Petra Krizan <petrakrizan27@gmail.com>
2018-09-06 11:14:49 +0200

    Initial commit, readme + impl.

---
+++ README.md
@@ -0,0 +1,8 @@
+javascript-streaming-client-example
+===================================
+
+This is an example how to use (socket.io-client)[https://www.npmjs.com/package/socket.io-client
+to connect to our [DPP DataStreams](https://platform.dapowerplay.com/services/free).
+
+This project contains an example how to connect & subscribe to a stream, register a listener for e.g. 'trades'
+to recieve the data.
---
+++ app.js
@@ -0,0 +1,47 @@
+const stream = require('socket.io-client')('https://streams.dapowerplay.com?apikey=xxxxxxxx&apisecret=xxxxxxxx')
+
+stream.on('connect', () => {
+    console.log('connected')
+
+    stream.emit('streams', (err, streams) => {
+        //console.log(streams)
+    })
+
+    stream.emit('subscribe', [
+        {product: 'trades', exchange: 'bitstamp', base: 'BTC', quote: 'USD'},
+        {product: 'orderbooks_lv1', exchange: 'bitstamp', base: 'BTC', quote: 'USD'},
+        {product: 'candles', exchange: 'bitstamp', base: 'BTC', quote: 'USD'},
+        {product: 'news'}
+    ], (err,res) => {
+        console.log(err, res)
+    })
+})
+
+stream.on('trades', (trade) => {
+    // all trades that you subscribed for will be received here
+    console.log(trade)
+})
+
+stream.on('orderbooks_lv1', (orderbook) => {
+    // all orderbooks that you subscribed for will be received here
+    console.log(orderbook)
+})
+
+stream.on('candles', (candle) => {
+    // all candles that you subscribed for will be received here
+    console.log(candle)
+})
+
+stream.on('news', (news) => {
+    // all news that you subscribed for will be received here
+    console.log(news)
+})
+
+
+stream.on('connect_error', (err) => {
+    console.log(err)
+})
+
+stream.on('error', (err) => {
+    console.log(err)
+})
